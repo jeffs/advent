79de5f7e1c3b7afc30e3219db436e74ba1206fbb
Jeff Schwab <jeff@unbuggy.com>
2020-12-01 21:40:10 -0500

    Solve day 1 part 2

--- 2020/src/bin/day1.rs
+++ 2020/src/bin/day1.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fmt::{self, Debug, Display, Formatter};
 use std::fs::File;
@@ -16,45 +16,57 @@ impl Display for NoSolution {
 
 impl Error for NoSolution {}
 
-mod part1 {
-    use super::*;
-
-    fn load_entries<P: AsRef<Path>>(input: P) -> Result<HashSet<i32>, Box<dyn Error>> {
-        let mut entries = HashSet::new();
-        for line in BufReader::new(File::open(input)?).lines() {
-            entries.insert(line?.parse()?);
-        }
-        Ok(entries)
+// O(N) time, O(N) space
+fn load_entries<P: AsRef<Path>>(input: P) -> Result<HashSet<i32>, Box<dyn Error>> {
+    let mut entries = HashSet::new();
+    for line in BufReader::new(File::open(input)?).lines() {
+        entries.insert(line?.parse()?);
     }
-
-    pub fn solve<P: AsRef<Path>>(input: P) -> Result<i32, Box<dyn Error>> {
-        let entries = load_entries(input)?;
-        for entry in &entries {
-            let delta = 2020 - entry;
-            if entries.contains(&delta) {
-                return Ok(entry * delta);
-            }
-        }
-        Err(Box::new(NoSolution))
-    }
+    Ok(entries)
 }
 
-mod part2 {
-    use super::*;
-
-    pub fn solve<P: AsRef<Path>>(_input: P) -> Result<i32, Box<dyn Error>> {
-        todo!()
+// O(N) time, O(1) space
+pub fn solve_part1(entries: &HashSet<i32>) -> Result<i32, Box<dyn Error>> {
+    for entry in entries {
+        let delta = 2020 - entry;
+        if entries.contains(&delta) {
+            return Ok(delta * entry);
+        }
     }
+    Err(Box::new(NoSolution))
+}
+
+// O(N²) time, O(N²) space
+pub fn solve_part2(entries: &HashSet<i32>) -> Result<i32, Box<dyn Error>> {
+    let mut pairs = HashMap::new(); // O(N²) space
+    for first in entries {
+        for second in entries {
+            if first != second {
+                pairs.insert(first + second, (first, second));
+            }
+        }
+    }
+    for third in entries {
+        let delta = 2020 - third;
+        if let Some((&first, &second)) = pairs.get(&delta) {
+            return Ok(first * second * third);
+        }
+    }
+    Err(Box::new(NoSolution))
 }
 
 fn main() {
     let input = "tests/day1/input";
-    let answer1 = part1::solve(input).unwrap_or_else(|err| {
+    let entries = load_entries(input).unwrap_or_else(|err| {
+        eprintln!("error: {}: {}", input, err);
+        std::process::exit(3);
+    });
+    let answer1 = solve_part1(&entries).unwrap_or_else(|err| {
         eprintln!("error: {}", err);
         std::process::exit(1);
     });
     println!("{}", answer1);
-    let answer2 = part2::solve(input).unwrap_or_else(|err| {
+    let answer2 = solve_part2(&entries).unwrap_or_else(|err| {
         eprintln!("error: {}", err);
         std::process::exit(2);
     });
