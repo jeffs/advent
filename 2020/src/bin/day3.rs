33ac356df894a1ab99be7b31863303a5363b41d1
Jeff Schwab <jeff@unbuggy.com>
2020-12-03 08:47:41 -0500

    Replace hand-rolled advance() function with .nth()
    Derp.

--- 2020/src/bin/day3.rs
+++ 2020/src/bin/day3.rs
@@ -6,20 +6,13 @@ fn load_rows<P: AsRef<Path>>(input: P) -> io::Result<Vec<String>> {
     BufReader::new(File::open(input)?).lines().collect()
 }
 
-fn advance<'a, I: Iterator<Item = &'a String>>(iter: &mut I, n: usize) -> &mut I {
-    for _ in 0..n {
-        iter.next();
-    }
-    iter
-}
-
 fn solve(rows: &Vec<String>, right: usize, down: usize) -> i32 {
-    let mut iter = rows.iter().fuse();
+    let mut iter = rows.iter();
     let row = iter.next().expect("empty table");
     let width = row.len();
     let mut x = 0;
     let mut count = if row.as_bytes()[0] == b'#' { 1 } else { 0 };
-    while let Some(row) = advance(&mut iter, down - 1).next() {
+    while let Some(row) = iter.nth(down - 1) {
         x = (x + right) % width;
         if row.as_bytes()[x] == b'#' {
             count += 1
