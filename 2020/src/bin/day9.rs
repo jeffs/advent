e2cb81c220c604c6ece838384f87339081e7dc41
Jeff Schwab <jeff@unbuggy.com>
2020-12-09 08:50:38 -0500

    Solve day 9 part 2

--- 2020/src/bin/day9.rs
+++ 2020/src/bin/day9.rs
@@ -7,7 +7,7 @@ use std::path::Path;
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
-fn is_sum_of_any_pair(number: i32, past: &VecDeque<i32>) -> bool {
+fn is_sum_of_any_pair(number: usize, past: &VecDeque<usize>) -> bool {
     for m in past {
         for n in past {
             if m != n && m + n == number {
@@ -18,7 +18,7 @@ fn is_sum_of_any_pair(number: i32, past: &VecDeque<i32>) -> bool {
     false
 }
 
-fn read_preamble<I>(lines: I, size: usize) -> Result<VecDeque<i32>>
+fn read_preamble<I>(lines: I, size: usize) -> Result<VecDeque<usize>>
 where
     I: Iterator<Item = io::Result<String>>,
 {
@@ -29,9 +29,9 @@ where
     Ok(preamble)
 }
 
-// Reads numbers from the specified input file, beginning with a preamble of
-// the specified memory length.
-fn solve_part1<P>(input: P, memory: usize) -> Result<i32>
+/// Reads numbers from the specified input file, beginning with a preamble of
+/// the specified memory length.
+fn solve_part1<P>(input: P, memory: usize) -> Result<usize>
 where
     P: AsRef<Path>,
 {
@@ -48,9 +48,57 @@ where
     Err(Box::new(NoSolution))
 }
 
+fn read_numbers<P>(input: P) -> Result<Vec<usize>>
+where
+    P: AsRef<Path>,
+{
+    let mut numbers = Vec::new();
+    for line in BufReader::new(File::open(input)?).lines() {
+        numbers.push(line?.parse()?);
+    }
+    Ok(numbers)
+}
+
+/// Returns the sum of the smallest and largest of the specified numbers.
+fn sum_min_max(numbers: &[usize]) -> usize {
+    let min = numbers.iter().min().unwrap();
+    let max = numbers.iter().max().unwrap();
+    return min + max;
+}
+
+/// Returns the sum of the first and last numbers of a contiguous subsequence
+/// from the specified input file that adds up to the specified series.
+fn solve_part2<P>(input: P, series: usize) -> Result<usize>
+where
+    P: AsRef<Path>,
+{
+    let numbers = read_numbers(input)?;
+    let sums: Vec<usize> = numbers // sum of numbers up to each index, inclusive
+        .iter()
+        .scan(0, |sum, number| {
+            *sum += number;
+            Some(*sum)
+        })
+        .collect();
+    for end in 1..sums.len() {
+        if sums[end] == series {
+            println!("HERE");
+            return Ok(sum_min_max(&numbers[0..end]));
+        }
+        for begin in 0..end {
+            if sums[end] - sums[begin] == series {
+                return Ok(sum_min_max(&numbers[(begin + 1)..end]));
+            }
+        }
+    }
+    Err(Box::new(NoSolution))
+}
+
 fn main() {
     let input = "tests/day9/input";
-    println!("{}", solve_part1(input, 25).unwrap());
+    let answer1 = solve_part1(input, 25).unwrap();
+    println!("{}", answer1);
+    println!("{}", solve_part2(input, answer1).unwrap());
 }
 
 #[cfg(test)]
@@ -61,5 +109,11 @@ mod test {
     fn sample1() {
         let input = "tests/day9/sample1";
         assert_eq!(127, solve_part1(input, 5).unwrap());
+    }
+
+    #[test]
+    fn sample2() {
+        let input = "tests/day9/sample2";
+        assert_eq!(62, solve_part2(input, 127).unwrap());
     }
 }
