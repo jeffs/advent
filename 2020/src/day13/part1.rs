ecb7cc0a8efbf936f32ac1f75a9ab8d25d4f5de4
Jeff Schwab <jeff@unbuggy.com>
2020-12-13 17:52:35 -0500

    Factor day 13 part 1 into library

---
+++ 2020/src/day13/part1.rs
@@ -0,0 +1,64 @@
+use crate::error::{EmptyFile, ParseError};
+use std::error::Error;
+use std::fs;
+
+#[derive(Debug)]
+struct Input {
+    timestamp: usize,
+    buses: Vec<usize>,
+}
+
+fn load_input(input_path: &str) -> Result<Input, Box<dyn Error>> {
+    let text = fs::read_to_string(input_path)?;
+    let mut lines = text.split_terminator('\n');
+    let timestamp: usize = lines
+        .next()
+        .ok_or_else(|| EmptyFile::new(input_path))?
+        .parse()?;
+    let buses: Vec<usize> = lines
+        .next()
+        .ok_or_else(|| {
+            let what = format!("{}: expected two lines, got only one", input_path);
+            ParseError::new(what)
+        })?
+        .split_terminator(',')
+        .filter_map(|s| s.parse().ok())
+        .collect();
+    if lines.next().is_some() {
+        let what = format!("{}: expected only two lines", input_path);
+        Err(Box::new(ParseError::new(what)))
+    } else if buses.is_empty() {
+        let what = format!("{}: can't find any buses", input_path);
+        Err(Box::new(ParseError::new(what)))
+    } else {
+        Ok(Input { timestamp, buses })
+    }
+}
+
+pub fn solve(input_path: &str) -> Result<usize, Box<dyn Error>> {
+    let input = load_input(input_path)?;
+    if let Some(_) = input.buses.iter().find(|&&bus| input.timestamp % bus == 0) {
+        Ok(0)
+    } else {
+        let to_etd = |bus| bus * (input.timestamp / bus + 1);
+        let etd = input.buses.iter().map(to_etd).min().unwrap();
+        let index = input
+            .buses
+            .iter()
+            .map(to_etd)
+            .position(|t| t == etd)
+            .unwrap();
+        let bus = input.buses[index];
+        Ok(bus * (etd - input.timestamp))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn sample1() {
+        assert_eq!(295, solve("tests/day13/sample1").unwrap());
+    }
+}
--- 2020/src/lib.rs
+++ 2020/src/lib.rs
@@ -2,5 +2,6 @@ pub mod day7;
 
 pub mod day11;
 pub mod day12;
+pub mod day13;
 
 pub mod error;
