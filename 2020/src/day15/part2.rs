cf560eed92d0a18c6589d5a171d9132866a04270
Jeff Schwab <jeff@unbuggy.com>
2020-12-16 06:52:29 -0500

    Don't run slow tests by default
    To run all tests, use `cargo test -- --ignored`.

--- 2020/src/day15/part2.rs
+++ 2020/src/day15/part2.rs
@@ -14,13 +14,44 @@ mod test {
     use super::*;
 
     #[test]
-    fn solve_samples() {
+    #[ignore]
+    fn solve036() {
         assert_eq!(175594, solve(&[0, 3, 6]));
-        //assert_eq!(2578, solve(&[1, 3, 2]));
-        //assert_eq!(3544142, solve(&[2, 1, 3]));
-        //assert_eq!(261214, solve(&[1, 2, 3]));
-        //assert_eq!(6895259, solve(&[2, 3, 1]));
-        //assert_eq!(18, solve(&[3, 2, 1]));
-        //assert_eq!(362, solve(&[3, 1, 2]));
+    }
+
+    #[test]
+    #[ignore]
+    fn solve132() {
+        assert_eq!(2578, solve(&[1, 3, 2]));
+    }
+
+    #[test]
+    #[ignore]
+    fn solve213() {
+        assert_eq!(3544142, solve(&[2, 1, 3]));
+    }
+
+    #[test]
+    #[ignore]
+    fn solve123() {
+        assert_eq!(261214, solve(&[1, 2, 3]));
+    }
+
+    #[test]
+    #[ignore]
+    fn solve231() {
+        assert_eq!(6895259, solve(&[2, 3, 1]));
+    }
+
+    #[test]
+    #[ignore]
+    fn solve321() {
+        assert_eq!(18, solve(&[3, 2, 1]));
+    }
+
+    #[test]
+    #[ignore]
+    fn solve312() {
+        assert_eq!(362, solve(&[3, 1, 2]));
     }
 }
