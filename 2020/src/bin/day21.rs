588c0a352eb46455ca3eaceba9a8bdad56102150
Jeff Schwab <jeff@unbuggy.com>
2020-12-21 05:14:16 -0500

    Solve day 21 part 2

--- 2020/src/bin/day21.rs
+++ 2020/src/bin/day21.rs
@@ -2,6 +2,12 @@ use advent2020::error::ParseError;
 use std::collections::{HashMap, HashSet};
 use std::fs;
 
+struct Analysis<'a> {
+    candidates: HashMap<&'a str, HashSet<&'a str>>,
+    counts: HashMap<&'a str, usize>, // ingredient => number of appearances
+    safe: HashSet<&'a str>,          // ingredients that definitely have no allergens
+}
+
 fn parse_line(line: &str) -> Result<(HashSet<&str>, HashSet<&str>), ParseError> {
     let parts: Vec<_> = line.trim_end_matches(')').split(" (contains ").collect();
     match parts.as_slice() {
@@ -23,7 +29,7 @@ fn extend_set<'a>(mut target: HashSet<&'a str>, source: &HashSet<&'a str>) -> Ha
     target
 }
 
-fn solve_part1(text: &str) -> Result<usize, ParseError> {
+fn analyze(text: &str) -> Result<Analysis, ParseError> {
     let mut candidates: HashMap<&str, HashSet<&str>> = HashMap::new();
     let mut counts = HashMap::new();
     for line in text.lines() {
@@ -42,13 +48,51 @@ fn solve_part1(text: &str) -> Result<usize, ParseError> {
     let all_ingredients: HashSet<_> = counts.keys().cloned().collect();
     let dangerous = candidates.values().fold(HashSet::new(), extend_set);
     let safe = &all_ingredients - &dangerous;
+    Ok(Analysis {
+        candidates,
+        counts,
+        safe,
+    })
+}
+
+fn solve_part1(analysis: &Analysis) -> Result<usize, ParseError> {
+    let Analysis { counts, safe, .. } = analysis;
     Ok(safe.iter().map(|ingredient| counts[ingredient]).sum())
 }
 
+fn solve_part2(analysis: &Analysis) -> String {
+    let Analysis {
+        candidates, safe, ..
+    } = analysis;
+    let mut candidates = candidates.clone();
+    for set in candidates.values_mut() {
+        *set = &*set - safe;
+    }
+    let mut pairs: Vec<(&str, &str)> = Vec::new(); // (allergen, ingredient)...
+    while !candidates.is_empty() {
+        let known: HashMap<&str, &str> = candidates // allergen => ingredient
+            .iter()
+            .filter(|(_, set)| set.len() == 1)
+            .map(|(allergen, set)| (allergen.clone(), set.iter().next().unwrap().clone()))
+            .collect();
+        candidates.retain(|_, set| set.len() > 1);
+        for set in candidates.values_mut() {
+            for ingredient in known.values() {
+                set.remove(ingredient);
+            }
+        }
+        pairs.extend(known);
+    }
+    pairs.sort();
+    let ingredients: Vec<_> = pairs.iter().map(|pair| pair.1).collect();
+    ingredients.join(",")
+}
+
 fn main() {
-    let input_path = "tests/day21/input";
-    let text = fs::read_to_string(input_path).unwrap();
-    println!("{}", solve_part1(&text).unwrap());
+    let text = fs::read_to_string("tests/day21/input").unwrap();
+    let analysis = analyze(&text).unwrap();
+    println!("{}", solve_part1(&analysis).unwrap());
+    println!("{}", solve_part2(&analysis));
 }
 
 #[cfg(test)]
@@ -56,9 +100,16 @@ mod test {
     use super::*;
 
     #[test]
-    fn solve_sample1() {
-        let input_path = "tests/day21/sample1";
-        let text = fs::read_to_string(input_path).unwrap();
-        assert_eq!(5, solve_part1(&text).unwrap());
+    fn solve_part1_sample1() {
+        let text = fs::read_to_string("tests/day21/sample1").unwrap();
+        let analysis = analyze(&text).unwrap();
+        assert_eq!(5, solve_part1(&analysis).unwrap());
+    }
+
+    #[test]
+    fn solve_part2_sample1() {
+        let text = fs::read_to_string("tests/day21/sample1").unwrap();
+        let analysis = analyze(&text).unwrap();
+        assert_eq!("mxmxvkd,sqjhc,fvjkl", solve_part2(&analysis));
     }
 }
