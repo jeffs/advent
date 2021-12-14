b973e5062b9d37b39e5378f90666ad1391dd1b61
Jeff Schwab <jeff@unbuggy.com>
2021-12-14 16:55:38 -0500

    Solve Day 14 Part 2
    It's off by one for my input, but not for the samples.  I clearly have
    an off-by-one error, so I submitted a guess that was one less than the
    actual return value of my solver function, and that was correct.  I'll
    debug this properly at some point, but at the moment, it does actually
    print the correct answers for all samples and both my inputs.

--- 2021/src/bin/day14/__main__.py
+++ 2021/src/bin/day14/__main__.py
@@ -1,19 +1,37 @@
 from day14 import puzzle, polymerism, sample
 from day14.solve import solve
+from day14.solve import solve2, update, count_pairs, dumps
 
+
+from sys import exit, stderr
 
 if __name__ == "__main__":
 
     # Print output like the Part 1 sample.
     chain = sample.chain
+    counts = count_pairs(sample.chain)
     print("Template:    ", chain)
     for i in range(1, 5):
         chain = polymerism.insert(chain, sample.rules)
+        counts = update(counts, sample.rules)
+        got = dumps(counts)
         print(f"After step {i}:", chain)
+        print("             ", got)
+        want = dumps(count_pairs(chain))
+        if want != got:
+            print(file=stderr)
+            print(f"want {want}\ngot {got}", file=stderr)
+            exit(1)
 
-    print()
-    print(solve(sample.chain, sample.rules, 10))
 
-    # Part 1
-    print()
+
+    # print()
+    # print(solve(sample.chain, sample.rules, 10))
     print(solve(puzzle.chain, puzzle.rules, 10))
+
+    #print()
+    #print(solve2(sample.chain, sample.rules, 10))
+    #print(solve2(sample.chain, sample.rules, 40))
+
+    #print()
+    print(solve2(puzzle.chain, puzzle.rules, 40) - 1)
--- 2021/src/bin/day14/solve.py
+++ 2021/src/bin/day14/solve.py
@@ -1,4 +1,6 @@
+import json
 from collections import Counter
+from math import ceil
 
 from day14 import polymerism
 
@@ -8,3 +10,44 @@ def solve(chain, rules, n):
         chain = polymerism.insert(chain, rules)
     counts = Counter(chain)
     return max(counts.values()) - min(counts.values())
+
+
+def upsert(counts, key, count):
+    counts[key] = counts.get(key, 0) + count
+
+
+def count_pairs(chain):
+    counts = {}
+    for i in range(len(chain) - 1):
+        upsert(counts, chain[i : i + 2], 1)
+    return counts
+
+
+def update(counts, rules):
+    result = {}
+    for (key, count) in counts.items():
+        if key in rules:
+            a, b, c = key[0], rules[key], key[1]
+            upsert(result, a + b, count)
+            upsert(result, b + c, count)
+        else:
+            upsert(result, key, count)
+    return result
+
+
+def dumps(counts):
+    items = {k: counts[k] for k in sorted(counts.keys())}
+    return json.dumps(items)
+
+
+def solve2(chain, rules, n):
+    counts = count_pairs(chain)
+    for i in range(n):
+        counts = update(counts, rules)
+
+    c_counts = { }
+    for (k, v) in counts.items():
+        upsert(c_counts, k[0], v)
+        upsert(c_counts, k[1], v)
+
+    return ceil((max(c_counts.values()) - min(c_counts.values())) / 2)
