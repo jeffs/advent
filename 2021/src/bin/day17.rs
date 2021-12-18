ea2bcd7a84a05b8009a0c870589eb3f745f6021c
Jeff Schwab <jeff@unbuggy.com>
2021-12-17 20:26:25 -0500

    Solve 2021 Day 17 Part 2

--- 2021/src/bin/day17.rs
+++ 2021/src/bin/day17.rs
@@ -70,6 +70,14 @@ impl Target {
         None
     }
 
+    pub fn paths(&self) -> impl Iterator<Item = Vec<(i32, i32)>> + '_ {
+        let (r, b) = (self.right(), self.bottom());
+        (b..-b)
+            .rev()
+            .flat_map(move |vy| (-r..r).map(move |vx| (vx, vy)))
+            .filter_map(|(vx, vy)| self.path(vx, vy))
+    }
+
     pub fn print(&self, path: &[(i32, i32)]) {
         println!("\n{}\n", self.render(path));
     }
@@ -121,19 +129,16 @@ impl Display for Target {
     }
 }
 
-pub mod part1 {
-    use super::*;
+fn path_height(path: &[(i32, i32)]) -> i32 {
+    *path.iter().map(|(_, y)| y).max().expect("empty path")
+}
 
-    fn path_height(path: &[(i32, i32)]) -> i32 {
-        *path.iter().map(|(_, y)| y).max().expect("empty path")
-    }
+pub mod part1 {
+    use super::{path_height, Target};
 
     pub fn solve(target: &Target) -> i32 {
-        let (r, b) = (target.right(), target.bottom());
-        let path = (0..-b)
-            .rev()
-            .flat_map(move |vy| (0..r).map(move |vx| (vx, vy)))
-            .filter_map(|(vx, vy)| target.path(vx, vy))
+        let path = target
+            .paths()
             .max_by_key(|path| path_height(path))
             .expect("no solution");
         path_height(&path)
@@ -141,27 +146,75 @@ pub mod part1 {
 
     #[cfg(test)]
     mod tests {
-        use super::*;
-
-        #[test]
-        fn test_path72() {
-            let target = Target::from_file("tests/day17/sample").unwrap();
-            let path = target.path(7, 2).expect("can't find sample path");
-            assert_eq!(8, path.len());
-            assert_eq!(3, path_height(&path));
-        }
-
-        #[test]
-        fn test_path69() {
-            let target = Target::from_file("tests/day17/sample").unwrap();
-            let path = target.path(6, 9).expect("can't find sample path");
-            assert_eq!(45, path_height(&path));
-        }
+        use super::{solve, Target};
 
         #[test]
         fn test_solve() {
             let target = Target::from_file("tests/day17/sample").unwrap();
             assert_eq!(45, solve(&target));
+        }
+    }
+}
+
+pub mod part2 {
+    use super::*;
+
+    pub fn solve(target: &Target) -> usize {
+        target.paths().count()
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::{solve, Target};
+
+        #[test]
+        fn test_solve() {
+            let target = Target::from_file("tests/day17/sample").unwrap();
+            assert_eq!(112, solve(&target));
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{path_height, Target};
+
+    #[test]
+    fn test_path72() {
+        let target = Target::from_file("tests/day17/sample").unwrap();
+        let path = target.path(7, 2).expect("can't find sample path");
+        assert_eq!(8, path.len());
+        assert_eq!(3, path_height(&path));
+    }
+
+    #[test]
+    fn test_path69() {
+        let target = Target::from_file("tests/day17/sample").unwrap();
+        let path = target.path(6, 9).expect("can't find sample path");
+        assert_eq!(45, path_height(&path));
+    }
+
+    #[test]
+    fn test_path_velocities() {
+        #[rustfmt::skip]
+        let want = [
+            (23, -10), (25, -9), (27, -5), (29, -6), (22, -6), (21, -7), (9, 0), (27, -7), (24, -5),
+            (25, -7), (26, -6), (25, -5), (6, 8), (11, -2), (20, -5), (29, -10), (6, 3), (28, -7),
+            (8, 0), (30, -6), (29, -8), (20, -10), (6, 7), (6, 4), (6, 1), (14, -4), (21, -6),
+            (26, -10), (7, -1), (7, 7), (8, -1), (21, -9), (6, 2), (20, -7), (30, -10), (14, -3),
+            (20, -8), (13, -2), (7, 3), (28, -8), (29, -9), (15, -3), (22, -5), (26, -8), (25, -8),
+            (25, -6), (15, -4), (9, -2), (15, -2), (12, -2), (28, -9), (12, -3), (24, -6), (23, -7),
+            (25, -10), (7, 8), (11, -3), (26, -7), (7, 1), (23, -9), (6, 0), (22, -10), (27, -6),
+            (8, 1), (22, -8), (13, -4), (7, 6), (28, -6), (11, -4), (12, -4), (26, -9), (7, 4),
+            (24, -10), (23, -8), (30, -8), (7, 0), (9, -1), (10, -1), (26, -5), (22, -9), (6, 5),
+            (7, 5), (23, -6), (28, -10), (10, -2), (11, -1), (20, -9), (14, -2), (29, -7), (13, -3),
+            (23, -5), (24, -8), (27, -9), (30, -7), (28, -5), (21, -10), (7, 9), (6, 6), (21, -5),
+            (27, -10), (7, 2), (30, -9), (21, -8), (22, -7), (24, -9), (20, -6), (6, 9), (29, -5),
+            (8, -2), (27, -8), (30, -5), (24, -7),
+        ];
+        let target = Target::from_file("tests/day17/sample").unwrap();
+        for (vx, vy) in want {
+            assert!(target.path(vx, vy).is_some())
         }
     }
 }
@@ -173,4 +226,5 @@ fn main() {
         std::process::exit(3);
     });
     println!("{}", part1::solve(&target));
+    println!("{}", part2::solve(&target));
 }
