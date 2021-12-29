3a7cc5b1acc58ccf353d3a2713406e46c1918f80
Jeff Schwab <jeff@unbuggy.com>
2021-12-29 15:09:23 -0500

    Solve 2021 Day 20 Part 2

--- 2021/src/bin/day20/main.rs
+++ 2021/src/bin/day20/main.rs
@@ -26,6 +26,30 @@ pub mod part1 {
     }
 }
 
+pub mod part2 {
+    use super::*;
+
+    pub fn solve(puzzle: &Puzzle) -> Result<usize, &'static str> {
+        let algo = &puzzle.algo;
+        let mut image = algo.enhance(&puzzle.image);
+        for _ in 0..49 {
+            image = algo.enhance(&image);
+        }
+        image.count_lights()
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::{solve, Puzzle};
+
+        #[test]
+        fn test_solve() {
+            let puzzle = Puzzle::from_file("tests/day20/sample").unwrap();
+            assert_eq!(3351, solve(&puzzle).unwrap());
+        }
+    }
+}
+
 fn main() {
     let input = "tests/day20/input";
     let puzzle = Puzzle::from_file(input).unwrap_or_else(|err| {
@@ -33,6 +57,13 @@ fn main() {
         std::process::exit(3);
     });
     match part1::solve(&puzzle) {
+        Ok(answer) => println!("{}", answer),
+        Err(err) => {
+            eprintln!("error: {}", err);
+            std::process::exit(1);
+        }
+    }
+    match part2::solve(&puzzle) {
         Ok(answer) => println!("{}", answer),
         Err(err) => {
             eprintln!("error: {}", err);
