2b29341f0ae7357448c3617e26760953e1aec6c7
Jeff Schwab <jeff@unbuggy.com>
2022-12-03 23:34:36 -0500

    Solve 2022 day 3 part 2

--- 2022/src/bin/day3.rs
+++ 2022/src/bin/day3.rs
@@ -5,17 +5,17 @@ use std::io::{BufRead as _, BufReader};
 use std::path::Path;
 use std::process::exit;
 
+fn item_to_index(item: u8) -> Result<usize, BoxedError> {
+    let index = match item {
+        b'a'..=b'z' => item - b'a',
+        b'A'..=b'Z' => item - b'A' + 26,
+        _ => return Err(StaticError::boxed("bad item")),
+    };
+    Ok(index as usize)
+}
+
 pub mod part1 {
     use super::*;
-
-    fn item_to_index(item: u8) -> Result<usize, BoxedError> {
-        let index = match item {
-            b'a'..=b'z' => item - b'a',
-            b'A'..=b'Z' => item - b'A' + 26,
-            _ => return Err(StaticError::boxed("bad item")),
-        };
-        Ok(index as usize)
-    }
 
     pub fn solve(path: impl AsRef<Path>) -> Result<u64, BoxedError> {
         let mut sum = 0;
@@ -60,9 +60,48 @@ pub mod part1 {
         }
     }
 }
+pub mod part2 {
+    use super::*;
+
+    pub fn solve(path: impl AsRef<Path>) -> Result<u64, BoxedError> {
+        let mut sum = 0;
+        let file = File::open(path)?;
+        let mut lines = BufReader::new(file).lines();
+        while let (Some(s), Some(t), Some(u)) = (lines.next(), lines.next(), lines.next()) {
+            let s_set: HashSet<u8> = s?.bytes().collect();
+            let t_set: HashSet<u8> = t?.bytes().collect();
+            let u_set: HashSet<u8> = u?.bytes().collect();
+            let Some(&item) = s_set.iter().filter(|b|
+                t_set.contains(b) && u_set.contains(b)
+            ).next() else {
+                return Err(StaticError::boxed("can't find badge"));
+            };
+            let priority = item_to_index(item)? as u64 + 1;
+            sum += priority;
+        }
+        Ok(sum)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_solve_sample() {
+            let answer = solve("tests/day3/sample").expect("sample should have an answer");
+            assert_eq!(70, answer);
+        }
+
+        #[test]
+        fn test_solve_input() {
+            let answer = solve("tests/day3/input").expect("input should have an answer");
+            assert_eq!(2607, answer);
+        }
+    }
+}
 
 fn main() {
-    for solve in [part1::solve] {
+    for solve in [part1::solve, part2::solve] {
         let answer = solve("tests/day3/input").unwrap_or_else(|err| {
             eprintln!("error: {err}");
             exit(1);
