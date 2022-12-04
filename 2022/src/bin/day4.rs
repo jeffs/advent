c1a6c0989d2a5c4a0c23ec18347141bbf4141561
Jeff Schwab <jeff@unbuggy.com>
2022-12-04 09:21:11 -0500

    Solve 2022 day 4 part 2

--- 2022/src/bin/day4.rs
+++ 2022/src/bin/day4.rs
@@ -10,35 +10,43 @@ struct Assignment {
 }
 
 impl Assignment {
+    fn contains(&self, section: u64) -> bool {
+        self.first <= section && section <= self.last
+    }
+
     fn fully_contains(&self, other: &Assignment) -> bool {
         self.first <= other.first && other.last <= self.last
     }
+
+    fn overlaps(&self, other: &Assignment) -> bool {
+        self.contains(other.first) || self.contains(other.last) || other.fully_contains(self)
+    }
+}
+
+fn parse_range(range: &str) -> Result<Assignment, BoxedError> {
+    let mut split = range.split('-');
+    let pair = match (split.next(), split.next(), split.next()) {
+        (Some(first), Some(last), None) => (first, last),
+        _ => return Err(StaticError::boxed("bad range")),
+    };
+    let first = pair.0.parse()?;
+    let last = pair.1.parse()?;
+    Ok(Assignment { first, last })
+}
+
+fn parse_range_pair(line: &str) -> Result<(Assignment, Assignment), BoxedError> {
+    let mut split = line.split(',');
+    let ranges = match (split.next(), split.next(), split.next()) {
+        (Some(first), Some(second), None) => (first, second),
+        _ => return Err(StaticError::boxed("bad range pair")),
+    };
+    let first = parse_range(ranges.0)?;
+    let second = parse_range(ranges.1)?;
+    Ok((first, second))
 }
 
 pub mod part1 {
     use super::*;
-
-    fn parse_range(range: &str) -> Result<Assignment, BoxedError> {
-        let mut split = range.split('-');
-        let pair = match (split.next(), split.next(), split.next()) {
-            (Some(first), Some(last), None) => (first, last),
-            _ => return Err(StaticError::boxed("bad range")),
-        };
-        let first = pair.0.parse()?;
-        let last = pair.1.parse()?;
-        Ok(Assignment { first, last })
-    }
-
-    fn parse_range_pair(line: &str) -> Result<(Assignment, Assignment), BoxedError> {
-        let mut split = line.split(',');
-        let ranges = match (split.next(), split.next(), split.next()) {
-            (Some(first), Some(second), None) => (first, second),
-            _ => return Err(StaticError::boxed("bad range pair")),
-        };
-        let first = parse_range(ranges.0)?;
-        let second = parse_range(ranges.1)?;
-        Ok((first, second))
-    }
 
     pub fn solve(path: impl AsRef<Path>) -> Result<u64, BoxedError> {
         let mut sum = 0;
@@ -62,16 +70,49 @@ pub mod part1 {
             assert_eq!(2, answer);
         }
 
-        //#[test]
-        // fn test_solve_input() {
-        //     let answer = solve("tests/day4/input").expect("input should have an answer");
-        //     assert_eq!(7597, answer);
-        // }
+        #[test]
+        fn test_solve_input() {
+            let answer = solve("tests/day4/input").expect("input should have an answer");
+            assert_eq!(466, answer);
+        }
+    }
+}
+
+pub mod part2 {
+    use super::*;
+
+    pub fn solve(path: impl AsRef<Path>) -> Result<u64, BoxedError> {
+        let mut sum = 0;
+        let file = File::open(path)?;
+        for line in BufReader::new(file).lines() {
+            let (first, second) = parse_range_pair(&line?)?;
+            if first.overlaps(&second) {
+                sum += 1;
+            }
+        }
+        Ok(sum)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_solve_sample() {
+            let answer = solve("tests/day4/sample").expect("sample should have an answer");
+            assert_eq!(4, answer);
+        }
+
+        #[test]
+        fn test_solve_input() {
+            let answer = solve("tests/day4/input").expect("input should have an answer");
+            assert_eq!(865, answer);
+        }
     }
 }
 
 fn main() {
-    for solve in [part1::solve] {
+    for solve in [part1::solve, part2::solve] {
         let answer = solve("tests/day4/input").unwrap_or_else(|err| {
             eprintln!("error: {err}");
             exit(1);
