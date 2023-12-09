87777d6f94b5ff3d09af1799ffbe90e9e1facb7d
Jeff Schwab <jeff@unbuggy.com>
2023-12-09 05:29:25 -0500

    Solve 2023 day 9 part 1

--- 2023/day9/src/part1.rs
+++ 2023/day9/src/part1.rs
@@ -1,9 +1,26 @@
-pub fn solve_line(_text: &str) -> i32 {
-    todo!()
+fn retain_deltas(mut values: Vec<i32>) -> Vec<i32> {
+    for i in 1..values.len() {
+        values[i - 1] = values[i] - values[i - 1];
+    }
+    values.pop();
+    values
 }
 
-pub fn solve(_text: &str) -> i32 {
-    todo!()
+pub fn solve_line(line: &str) -> i32 {
+    let mut values: Vec<i32> = line
+        .split(' ')
+        .map(|s| s.parse().expect("number"))
+        .collect();
+    let mut lasts: Vec<i32> = vec![];
+    while !values.iter().all(|&value| value == 0) {
+        lasts.push(*values.last().expect("loop to end when vec is empty"));
+        values = retain_deltas(values);
+    }
+    lasts.into_iter().sum()
+}
+
+pub fn solve(text: &str) -> i32 {
+    text.lines().map(solve_line).sum()
 }
 
 #[cfg(test)]
