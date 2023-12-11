b183166bbc782fff1bb783ec6d15b259a4572e1b
Jeff Schwab <jeff@unbuggy.com>
2023-12-11 08:27:31 -0500

    Flatten module structure; mods were almost empty

--- 2023/day11/src/main.rs
+++ 2023/day11/src/main.rs
@@ -1,5 +1,7 @@
+use day11::Grid;
+
 fn main() {
-    let text = include_str!("input.txt");
-    println!("{}", day11::part1::solve(text));
-    println!("{}", day11::part2::solve(text));
+    let grid = Grid::parse(include_str!("input.txt"));
+    println!("{}", grid.distance(2));
+    println!("{}", grid.distance(1000000));
 }
--- 2023/day11/src/part2.rs
+++
@@ -1,5 +0,0 @@
-use crate::grid::Grid;
-
-pub fn solve(text: &str) -> usize {
-    Grid::from_str(text).distance(1000000)
-}
