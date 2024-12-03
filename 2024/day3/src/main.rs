0e8c65049d5eced0283bc1db3586396d2b7ec6e0
Jeff Schwab <jeff@unbuggy.com>
2024-12-03 14:53:39 -0500

    Solve 2024 day 3 part 2

--- 2024/day3/src/main.rs
+++ 2024/day3/src/main.rs
@@ -1,6 +1,7 @@
-use day3::part1;
+use day3::{part1, part2};
 
 fn main() {
     let input = include_str!("input");
     println!("{}", part1::solve(input));
+    println!("{}", part2::solve(input));
 }
